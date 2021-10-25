a6ad268711da2dea3943d9bb600b1dabf7f98605
AlvaroPlazaSanz <55554546+AlvaroPlazaSanz@users.noreply.github.com>
2021-10-25 17:24:35 +0200

    Create ejercicio8.cc

---
+++ practica2.1/ejercicio8.cc
@@ -0,0 +1,11 @@
+
+
+> man id
+
+//Familiarizarse con el comando
+> id --group
+1000
+> id -u
+1000
+> id
+uid=1000(alvaplaz) gid=1000(alvaplaz) groups=1000(alvaplaz),4(adm),20(dialout),24(cdrom),25(floppy),27(sudo),29(audio),30(dip),44(video),46(plugdev),117(netdev) 
