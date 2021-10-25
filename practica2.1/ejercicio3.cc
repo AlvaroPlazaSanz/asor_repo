256ac75c59110595be8e4d46c4e6f7cf20e1f02e
AlvaroPlazaSanz <55554546+AlvaroPlazaSanz@users.noreply.github.com>
2021-10-25 13:59:54 +0200

    Update ejercicio3.cc

--- practica2.1/ejercicio3.cc
+++ practica2.1/ejercicio3.cc
@@ -1,1 +1,15 @@
+#include <unistd.h>                                                            
+#include <errno.h>                                                             
+#include <stdio.h>                                                             
+#include <string.h> 
 
+
+
+int main(){
+  
+  for(int i = 1; i <= 255; ++i)
+    printf("Información del error %i: %s\n", i, strerror(i)); 
+  
+  return 1;
+  
+}
