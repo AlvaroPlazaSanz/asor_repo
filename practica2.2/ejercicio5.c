981f9859fbe65a06791669956826e771bbf8c5cd
AlvaroPlazaSanz <55554546+AlvaroPlazaSanz@users.noreply.github.com>
2021-11-11 12:40:46 +0100

    Update ejercicio5.c

--- practica2.2/ejercicio5.c
+++ practica2.2/ejercicio5.c
@@ -1,19 +1,20 @@
-#include <sys/types.h>                                                              
-#include <sys/stat.h>                                                               
-#include <stdio.h>                                                                  
-#include <unistd.h>                                                                 
-#include <fcntl.h>  
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main(){                                                                                                                                                             
-                                                                   
-  int fd;                                                                                                                                                                                                                     
-  fd = open("ejercicio5.txt", O_WRONLY | O_CREAT | O_TRUNC, 0645);  
-  
-  if(fd==-1){                                                                                 
-    perror("Error al hacer el open");                                                   
-    return fd;                                                                  
-  }                                                                                                                                                                       
-  close(fd);                                                                          
-                                                                    
-  return 0;                                                                           
-}                                                                                                                              
+
+
+int main(void){
+
+        int fd = open("ej5", O_WRONLY | O_CREAT | O_TRUNC, 0645);                   
+        if(fd == -1){
+                perror("Error al hacer el open");
+                return fd;
+        }
+
+        close(fd);
+        return 0;
+}
+
