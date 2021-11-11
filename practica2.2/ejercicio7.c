ed05ab31543e9a167a79063088ec396951b8d4c1
AlvaroPlazaSanz <55554546+AlvaroPlazaSanz@users.noreply.github.com>
2021-11-11 13:06:23 +0100

    Update ejercicio7.c

--- practica2.2/ejercicio7.c
+++ practica2.2/ejercicio7.c
@@ -1,21 +1,15 @@
-#include <sys/types.h>                                                              
-#include <sys/stat.h>                                                               
-#include <unistd.h>                                                                 
-#include <fcntl.h>    
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 
-int main(void){       
-  
-  mode_t msc;                                                                         
-  msc = umask(0027);      
-  
-  int fd = open("ejer7", O_WRONLY | O_CREAT | O_TRUNC, 0645);                         
-  if(fd==-1){                                                                         
-    perror("Error al hacer el open");                                                   
-    return fd;     
-  }                                                                                   
-  
-  close(fd);                                                                          
-  umask(msc);                                                                         
-  return 0;      
-  
-}                                                                                     
+int main(void){
+
+        umask(0027);
+        int fd = open("ejercicio7", O_WRONLY | O_CREAT | O_TRUNC, 0645);
+        if(fd == -1) return fd;
+        close(fd);
+        return 0;
+}                                                                                 
