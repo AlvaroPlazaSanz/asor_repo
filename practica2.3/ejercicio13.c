c33799599c62768641d41d90ede50d17c39f2061
AlvaroPlazaSanz <55554546+AlvaroPlazaSanz@users.noreply.github.com>
2021-11-24 14:33:42 +0100

    Create ejercicio13.c

---
+++ practica2.3/ejercicio13.c
@@ -0,0 +1,47 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+
+
+
+volatile int borrar = 1;
+
+
+
+void suspender(int s){
+        borrar = 0;
+}
+
+
+
+
+int main(int argc, char **argv){
+
+        if (argc != 2) {
+                fprintf(stderr, "Usage: ./ej13 seconds\n");
+                exit(EXIT_FAILURE);
+         }
+
+
+        struct sigaction sigus;
+        long int s = strtol(argv[1], NULL, 10);
+        char *rm = strdup("rm ");
+        sigus.sa_handler = suspender;
+        sigus.sa_flags = SA_RESTART;
+        sigaction(SIGUSR1, &sigus, NULL);
+
+        printf("pid:%i\n", getpid());
+
+
+        sleep(s);
+        if(borrar){
+                strcat(rm,argv[0]);
+                system(rm);
+        }
+
+
+
+        return 0;
+}
