c5a95576a178f6fdf75cbc48a0c280d0e82c9768
AlvaroPlazaSanz <55554546+AlvaroPlazaSanz@users.noreply.github.com>
2021-12-22 18:58:10 +0100

    Update ejercicio2.c

--- practica2.5/ejercicio2.c
+++ practica2.5/ejercicio2.c
@@ -5,9 +5,44 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    int main(int argc, char *argv[]){                                                                                                                                                                                                                                                         struct addrinfo hints;                                                                                                                   struct addrinfo *res;                                                                                                                                                                                                                                                             int sd, rc;                                                                                                                                                                                                                                                                       //Control argumentos
-        if(argc != 3){                                                                                                                                   perror("Argumentos incorrectos.\n");                                                                                                     return -1;                                                                                                                       }                                                                                                                                                                                                                                                                                 memset(&hints, 0, sizeof(struct addrinfo));                                                                                                                                                                                                                                       hints.ai_family = AF_UNSPEC;                                                                                                             hints.ai_socktype = SOCK_DGRAM;                                                                                                                                                                                                                                                   //Control errores                                                                                                                        if((rc = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0){                                                                                     perror("Error al hacer el getaddrinfo().\n");                                                                                            return -1;                                                                                                                       }
-                                                                                                                                                 if((sd = socket(res->ai_family, res->ai_socktype, 0)) == -1){                                                                                    perror("Error al hacer el socket().\n");                                                                                                 return -1;                                                                                                                       }                                                                                                                                                                                                                                                                                 if(bind(sd, (struct sockaddr *) res->ai_addr, res->ai_addrlen) == -1){                                                                           perror("Error al hacer el bind().\n");                                                                                                   return -1;                                                                                                                       }                                                                                                                                
+
+
+
+
+int main(int argc, char *argv[]){
+
+        struct addrinfo hints;
+        struct addrinfo *res;
+
+        int sd, rc;
+
+        //Control argumentos
+        if(argc != 3){
+                perror("Argumentos incorrectos.\n");
+                return -1;
+        }
+
+        memset(&hints, 0, sizeof(struct addrinfo));
+
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_DGRAM;
+
+        //Control errores
+        if((rc = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0){
+                perror("Error al hacer el getaddrinfo().\n");
+                return -1;
+        }
+
+        if((sd = socket(res->ai_family, res->ai_socktype, 0)) == -1){
+                perror("Error al hacer el socket().\n");
+                return -1;
+        }
+
+        if(bind(sd, (struct sockaddr *) res->ai_addr, res->ai_addrlen) == -1){
+                perror("Error al hacer el bind().\n");
+                return -1;
+        }
+
         freeaddrinfo(res);
 
 
@@ -15,7 +50,8 @@
                 char buffer[8];
                 int n;
 
-                struct sockaddr_storage addr;                                                                                                            socklen_t addrlen = sizeof(struct sockaddr_storage);
+                struct sockaddr_storage addr;
+                socklen_t addrlen = sizeof(struct sockaddr_storage);
                 if((n = recvfrom(sd, buffer, 80, 0, (struct sockaddr *) &addr, &addrlen)) == -1){
                         perror(" Error al hacer el recvfrom().\n");
                         return -1;
@@ -26,7 +62,7 @@
                         perror("Error al hacer el getnameinfo()\n");
                         return -1;
                 }
-           printf("%d bytes de %s:%s\n", n, host, serv);
+                printf("%d bytes de %s:%s\n", n, host, serv);
 
 
                 if(strcmp(buffer, "t\n") == 0){
